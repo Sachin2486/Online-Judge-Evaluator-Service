2f723950a35f7519210248635daed1d20ef03b38
Sachin2486 <tiwari123sachin2@gmail.com>
2024-07-29 01:39:08 +0530

    BULL q queing mechanism setup is working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, {Express} from 'express';
 import serverConfig from './config/serverConfig';
 import apiRouter from './routes';
+import sampleQueueProducer from './producers/sampleQueueProducer';
+import SampleWorker from './workers/sampleWorker';
 
 const app: Express = express();
 
@@ -8,4 +10,13 @@ app.use('/api', apiRouter)
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at ${serverConfig.PORT}`);
+
+    SampleWorker('SampleQueue');
+
+    sampleQueueProducer('SampleJob',{
+        name:'Sachin',
+        company: 'ABC<XYZ',
+        Position: "SDE 1",
+        location: "Bangalore"
+    });
 });
---
+++ src/workers/sampleWorker.ts
@@ -0,0 +1,21 @@
+import { Worker, Job } from 'bullmq';
+import SampleJob from '../jobs/SampleJob';
+import redisConnection from '../config/redisConfig';
+
+export default function SampleWorker(queueName: string){
+    new Worker(
+        queueName, 
+        async(job: Job) => {
+            if(job.name == "SampleJob"){
+                const sampleJobInstance = new SampleJob(job.data);
+
+                sampleJobInstance.handle(job);
+
+                return true;
+            }
+        },
+        {
+            connection: redisConnection
+        }
+    );
+}
